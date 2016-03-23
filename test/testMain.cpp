23b475f953dd65a5ea1e9628fadf0b6c83349dd2
Andres Sevillano <andres.sevillano.vallejo@gmail.com>
2016-03-23 05:55:14 +0000

    Polished version into test messages

--- test/testMain.cpp
+++ test/testMain.cpp
@@ -2,9 +2,9 @@
 #define BOOST_TEST_STATIC_LINK
 #define BOOST_TEST_MODULE testMain
 #include <boost/test/unit_test.hpp>
-#include "../src/version.hpp"
+#include "version.hpp"
 
 BOOST_AUTO_TEST_CASE( testMain ) {
-    BOOST_TEST_MESSAGE( "TEST_CASE testMain " << VERSION_INFO );
+    BOOST_TEST_MESSAGE( "TEST_CASE testMain  on " << VERSION_INFO );
     BOOST_CHECK_EQUAL(1, 1);
 }
